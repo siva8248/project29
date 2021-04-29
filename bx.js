9686fec7a50ed9f2023c2cdc56a6e8edc32972bd
siva8248 <79700405+siva8248@users.noreply.github.com>
2021-04-29 21:14:59 +0530

    Add files via upload

---
+++ bx.js
@@ -0,0 +1,23 @@
+class b{
+        constructor(x, y, width, height) {
+          var options = {
+              isStatic: false,
+              restitution:0.8,
+              friction:0.3,
+              density:1.0
+          }
+          this.body = Bodies.rectangle(x, y, width, height, options);
+          this.width = width;
+          this.height = height;
+          World.add(world, this.body);
+        }
+        display(){
+          var pos =this.body.position;
+          push();
+          rectMode(CENTER);
+          stroke("black");
+          fill("blue");
+          rect(pos.x, pos.y, this.width, this.height);
+          pop();
+        }
+      };
